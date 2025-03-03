821f2d018c26fcd14dea6cfa9f39d531818ed917
elahe412 <67527897+elahe412@users.noreply.github.com>
2025-03-03 13:45:22 +0330

    Update urls.py

--- apps/hi/urls.py
+++ apps/hi/urls.py
@@ -5,4 +5,5 @@ from apps.hi.views import say_hello, say_my_name
 urlpatterns = [
     path('say_hello/', say_hello),
     path('say_my_name/', say_my_name),
+    path('random_response/',rando,_response),
 ]
