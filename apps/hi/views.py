cc33a7f7e76688bb53038869c0c3bfb33536876c
elahe412 <67527897+elahe412@users.noreply.github.com>
2025-03-03 13:50:53 +0330

    add random response

--- apps/hi/views.py
+++ apps/hi/views.py
@@ -14,3 +14,13 @@ def say_my_name(request):
     name = request.data.get('name', 'anonymous ')
     message = f'Hello {name}'
     return Response(message, status=HTTP_200_OK)
+
+
+@api_view(['POST'])
+def random_response(request):
+    status_code=request.data.get('status',200)
+    try:
+        return Response(f"message with {status_code}",status=status_code)
+    execpt: ValueError as e:
+        return Response(str(e,status=500)
+    
